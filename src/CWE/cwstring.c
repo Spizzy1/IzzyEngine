bdb2d60c70ba574c71d3c9d6defa6f93e9792714
Spizzy1 <isak.attila@gmail.com>
2024-04-18 13:13:35 +0200

    Syncing for work :)

--- src/CWE/cwstring.c
+++ src/CWE/cwstring.c
@@ -6,8 +6,9 @@ void sprint(struct String* str){
 
 struct String* substring(int start, int end, struct String* str){
     if(str->size < end){ 
-        
+                  
     }
     else{
+
     }
 }
